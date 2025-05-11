c3fa78871ed859dc281afd0e947b77ae99acb2db
Aarik Maharjan <mhrznaarik870@gmail.com>
2025-05-11 14:10:54 +0545

    updated configs of api for frontend

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    // return view('login');
+    // return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working!']);
+});
+Route::view('/login', 'login')->name('login');
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    server:{
-        proxy:{
-            "/api":{
-                target:"http://localhost:8000",
-                changeOrigin:true,
-                secure:false,
-            },
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
