2df2099f0fbf7dc0e7eed888fb188f3a5a819697
Aarik Maharjan <mhrznaarik870@gmail.com>
2025-05-12 12:57:06 +0545

    created migration files for districts, provinces and customers

---
+++ database/migrations/2025_05_11_090028_create_districts_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistrictsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('province_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('headquarters');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
